544c4c898d143054add54f2dd24422c2671f9e50
Vladyslav Yefremov <efr.vladislav@gmail.com>
2024-06-23 16:56:58 +0300

    hw25-ci-cd: add service and tests

---
+++ hw25-ci-cd/src/unit-tests/AnalyticsEventSummaryBuilderTests.cs
@@ -0,0 +1,41 @@
+using webapi.Data;
+
+namespace HW25.UnitTests;
+
+public class AnalyticsEventSummaryBuilderTests
+{
+    [Fact]
+    public void BuildSummary_ThrowsArgumentNullException_WhenEventsIsNull()
+    {
+        // Arrange
+        var builder = new AnalyticsEventSummaryBuilder();
+        
+        // Act
+        // ReSharper disable once ConvertToLocalFunction
+        Action act = () => builder.BuildSummary(null);
+        
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+    
+    [Fact]
+    public void BuildSummary_ReturnsSummaryWithCorrectCountAndEvents()
+    {
+        // Arrange
+        var events = new List<IAnalyticsEvent>
+        {
+            new AnalyticsEvent(),
+            new AnalyticsEvent(),
+            new AnalyticsEvent()
+        };
+        
+        var builder = new AnalyticsEventSummaryBuilder();
+        
+        // Act
+        var summary = builder.BuildSummary(events);
+        
+        // Assert
+        Assert.Equal(events.Count, summary.Count);
+        Assert.Equal(events, summary.Events);
+    }
+}
