539cb064227fafa38afe6450fff376604eedc190
Vladyslav Yefremov <efr.vladislav@gmail.com>
2024-06-29 12:45:49 +0300

    hw17-ddos: update

--- hw17-ddos/health.sh
+++ hw17-ddos/health.sh
@@ -6,17 +6,8 @@
 URL="http://localhost:8080/health"
 
 while true; do
-  # Capture the start time
-  START_TIME=$(date +%s%N)
-
   # Send the HTTP request and capture the response
   RESPONSE=$(curl -s -o /dev/null -w "Status %{http_code}, Time %{time_total}s" $URL)
-
-  # Capture the end time
-  END_TIME=$(date +%s%N)
-
-  # Calculate the response time in milliseconds
-  RESPONSE_TIME=$(echo "scale=3; ($END_TIME - $START_TIME) / 1000000" | bc)
 
   # Print the response
   echo "$(date +"%H:%M:%S"): ${RESPONSE}"
